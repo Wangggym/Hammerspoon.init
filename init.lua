479f8591e8f1f0e03816ec5be9eda506ff26a740
wangym <wangym0203@gmail.com>
2024-09-09 15:53:39 +0800

    add JSON to oneline function

--- init.lua
+++ init.lua
@@ -18,9 +18,48 @@ local flagsChangedTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, 
     return false
 end)
 
+local function judgeCodeEditorAndOthers(keyCode, alertMessage, keystroke)
+    local vscode = hs.application.find("Code")
+    local cursor = hs.application.find("Cursor")
+    if cursor and cursor:isRunning() then
+        codeEditor = cursor
+    elseif vscode and vscode:isRunning() then
+        codeEditor = vscode
+    end
+
+    if codeEditor then
+        hs.alert.show(alertMessage, 0.5)
+        codeEditor:activate()
+        hs.eventtap.keyStroke(table.unpack(keystroke))
+    else
+        hs.alert.show("Cursor 或 Visual Studio Code 未运行")
+    end
+    return true
+end
+
 -- 监听键盘按键事件
 local keyDownTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
     if rightCmdPressed then
+        if event:getKeyCode() == hs.keycodes.map.j then
+            local clipboard = hs.pasteboard.getContents()
+            if clipboard then
+                local success, result = pcall(function()
+                    local jsonObject = hs.json.decode(clipboard)
+                    local jsonString = hs.json.encode(jsonObject)
+                    hs.pasteboard.setContents(jsonString)
+                    return true
+                end)
+
+                if success then
+                    hs.alert.show("JSON object successfully converted to string and copied to clipboard", 1)
+                else
+                    hs.alert.show("Conversion failed: Clipboard content is not valid JSON", 2)
+                end
+            else
+                hs.alert.show("Clipboard is empty", 2)
+            end
+            return true
+        end
         if event:getKeyCode() == hs.keycodes.map.t then
             local chrome = hs.application.find("Chrome")
             if chrome then
@@ -35,14 +74,18 @@ local keyDownTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(e
         if event:getKeyCode() == hs.keycodes.map.r then
             local cursor = hs.application.find("Cursor")
             local vscode = hs.application.find("Code")
-            
-            if cursor or vscode then
-                local codeEditor = vscode or cursor
+            local codeEditor = nil
+            if cursor and cursor:isRunning() then
+                codeEditor = cursor
+            elseif vscode and vscode:isRunning() then
+                codeEditor = vscode
+            end
+            if codeEditor then
                 hs.alert.show("Command + R", 0.5)
                 previousApp = hs.application.frontmostApplication()
                 codeEditor:activate()
                 hs.eventtap.keyStroke({"shift", "cmd"}, "F5")
-                if previousApp ~= codeEditor then
+                if previousApp and previousApp ~= codeEditor then
                     hs.timer.doAfter(0.3, function()
                         previousApp:activate()
                         previousApp = nil
@@ -53,47 +96,17 @@ local keyDownTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(e
             end
             return true
         end
-        if event:getKeyCode() == hs.keycodes.map.n then
-            local vscode = hs.application.find("Code")
-            local cursor = hs.application.find("Cursor")
-            
-            if vscode or cursor then
-                local codeEditor = vscode or cursor
-                hs.alert.show("Command + N", 0.5)
-                codeEditor:activate()
-                hs.eventtap.keyStroke({"cmd"}, "N")
-            else
-                hs.alert.show("Cursor or Visual Studio Code is not running")
-            end
-            return true
-        end
-        if event:getKeyCode() == hs.keycodes.map.p then
-            local vscode = hs.application.find("Code")
-            local cursor = hs.application.find("Cursor")
 
-            if vscode or cursor then
-                local codeEditor = vscode or cursor
-                hs.alert.show("Command + P", 0.5)
-                codeEditor:activate()
-                hs.eventtap.keyStroke({"cmd"}, "P")
-            else
-                hs.alert.show("Cursor or Visual Studio Code is not running")
-            end
-            return true
+        if event:getKeyCode() == hs.keycodes.map.n then
+            return judgeCodeEditorAndOthers(hs.keycodes.map.n, "Command + N", {{"cmd"}, "N"})
         end
+
+        if event:getKeyCode() == hs.keycodes.map.p then
+            return judgeCodeEditorAndOthers(hs.keycodes.map.p, "Command + P", {{"cmd"}, "P"})
+        end
+
         if event:getKeyCode() == hs.keycodes.map.g then
-            local vscode = hs.application.find("Code")
-            local cursor = hs.application.find("Cursor")
-            
-            if vscode or cursor then
-                local codeEditor = vscode or cursor
-                hs.alert.show("Command + G", 0.5)
-                codeEditor:activate()
-                hs.eventtap.keyStroke({"option"}, "G")
-            else
-                hs.alert.show("Cursor or Visual Studio Code is not running")
-            end
-            return true
+            return judgeCodeEditorAndOthers(hs.keycodes.map.g, "Command + G", {{"option"}, "G"})
         end
 
     end
